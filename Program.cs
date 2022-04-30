5c68f1cd8f48172ad3d52ee9b3ca9ed6250e2b10
User <User@DELLVOSTRO1>
2022-05-01 00:17:32 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ia_ti_sala_din_prima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
